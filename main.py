8a1a78f7468b5f57b8322de30e2548c2a9aed3fa
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-08-01 09:32:15 -0400

    working routes (except /retweets)

--- main.py
+++ main.py
@@ -21,40 +21,40 @@ async def root():
 @app.get("/profile")
 async def root():
     id = 1721070150
-    res = twitter_client.get_user(id)
+    res = twitter_client.get_user(id=id, user_auth=True)
     return res
 
 
-@app.get("/comments")
-async def root():
-    id = 1553081717132611585
-    # res = twitter_client.GetReplies(id)
-    return res
+# @app.get("/comments")
+# async def root():
+#     id = 1553081717132611585
+#     # res = twitter_client.GetReplies(id)
+#     return res
 
 
 @app.get("/retweets")
 async def root():
     id = 1553081717132611585
-    res = twitter_client.retweet(id)
+    res = twitter_client.retweet(id, user_auth=True)
     return res
 
 
 @app.get("/tweetLikes")
 async def root():
     id = 1553081717132611585
-    res = twitter_client.get_liking_users(id)
+    res = twitter_client.get_liking_users(id=id, user_auth=True)
     return res
 
 
 @app.get("/userLikes")
 async def root():
     id = 1721070150
-    res = twitter_client.get_liked_tweets(id)
+    res = twitter_client.get_liked_tweets(id=id, user_auth=True)
     return res
 
 
 @app.get("/following")
 async def root():
     id = 1721070150
-    res = twitter_client.get_users_following(id)
+    res = twitter_client.get_users_following(id=id, user_auth=True)
     return res
